648cf00520a795101137d603b9747593c261a20b
josephatj <josephatjulius24@gmail.com>
2017-09-11 08:44:31 +0300

    adding the app to the github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import {OrgUnitFilterComponent} from "./components/org-unit-filter/org-unit-filter.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    OrgUnitFilterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/org-unit-filter/org-unit-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-org-unit-filter',
+  templateUrl: './org-unit-filter.component.html',
+  styleUrls: ['./org-unit-filter.component.css']
+})
+export class OrgUnitFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
